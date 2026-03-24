b7b4219d7e9dae8c825e12fffab19b5ccef37ffc
KIWOLY <ikiwoly@gmail.com>
2026-03-24 15:59:01 +0300

    add hero section part

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -24,6 +24,34 @@ const Hero = () => {
 
     </div>
     {/* content  */}
+    <div className='container mx-auto px-6 pt-32 pb-20 relative z-10'>
+        <div className='grid lg:grid-cols-2 gap-12 items-center'>
+            {/* left column-text content  */}
+            <div className='space-y-8'>
+                <div className='animate-fade-in'>
+                <span className='inline-flex items-center gap-2 px-4 py-2 rounded-full text-sm text-primary'>
+                  <span className='w-2 h-2 bg-primary rounded-b-full animate-pulse'/>
+                  Software engineer working with DevOps.
+                  </span>
+                </div>
+                {/* Headline */}
+                <div className='space-y-4'>
+                    <h1>
+                        Building <span className='text-primary glow-text'>applications</span>
+                        <br />
+                         and scalable
+                        <br />
+                        <span className='font-serif italic text-white font-normal'>DevOps solutions</span>
+                    </h1>
+                    <p className='text-lg text-muted-foreground max-w-lg animated-fade-in'>
+                      Hi, I'm Kiwoly, a software engineer working with DevOps.
+                       I build applications and focus on deploying, automating, and managing them efficiently
+                    </p>
+                </div>
+            </div>
+            {/* right column -profile picture */}
+        </div>
+    </div>
     
    </section>
   )
